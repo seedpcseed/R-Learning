9818be5142d26838224c685ae28a4789af626a75
seedpcseed <seedpcseed@gmail.com>
2020-06-05 07:42:32 -0500

    Create Script2.R

---
+++ R/Script2.R
@@ -0,0 +1,10 @@
+# R- Learning Script 2
+# June 5, 2020
+# P. Seed
+
+# Here we will do a little review of the steps in Script 1 session
+# We will then go beyond this to talk about 
+# Examining data.frames and matricies
+# Filtering and selecting parts 
+# Using basic piping
+
