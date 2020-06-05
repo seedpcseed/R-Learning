d5da8781145d4d3155a7e1db600b7a399a2f0280
seedpcseed <seedpcseed@gmail.com>
2020-06-05 07:48:13 -0500

    Update Script2.R

--- R/Script2.R
+++ R/Script2.R
@@ -8,3 +8,45 @@
 # Filtering and selecting parts 
 # Using basic piping
 
+#Get a datatable to play with and push around
+
+data(mtcars)
+
+head(mtcars)
+
+# Let's first look at what the objective class is
+
+class(mtcars)
+
+# Now assign this to a new variable
+
+df<- mtcars  #This will keep the original complete
+
+head(df) # Look like mtcars?
+
+class(df) # As expected the new variable takes on the class of the data assigned to it.
+
+# look at the dimensions of the dataframe
+
+nrows(df)
+
+ncols(df)
+
+# There is a way to get the dimensions in one command
+
+dim(df)  # this is showing the rows and columns
+
+# Remember how to get the column names? Rownames?
+
+colnames(df)
+
+rownames(df)
+
+# Now let's select just parts of the dataframe.
+# There are many ways in R to do this
+
+# Here is a classic way
+
+
+
+
