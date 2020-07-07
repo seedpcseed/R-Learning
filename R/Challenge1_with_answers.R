9c3a3c6138bb10df211e7016acf7f65c23f72a58
Patrick Seed <pcs3118@qanalytics02.ci.northwestern.edu>
2020-07-06 20:27:19 -0500

    Updated Challenge 1

--- R/Challenge1_with_answers.R
+++ R/Challenge1_with_answers.R
@@ -1,6 +1,7 @@
 # CHALLENGE: Plot the COVID-19 cases and deaths by   
 # 
 library(tidyverse)
+library(tibbletime)
 
 curl::curl_download("https://covid19-lake.s3.us-east-2.amazonaws.com/enigma-nytimes-data-in-usa/csv/us_states/us_states.csv", "Data/us_covid.csv")
 
@@ -11,16 +12,20 @@ midwest_states<- c("Illinois", "Indiana",
                    "North Dakota", "Ohio", 
                    "South Dakota", "Wisconsin")
 
-read.csv("Data/us_covid.csv") %>%
-  filter(date >= "2020-04-01" & date <= "2020-04-31" & state %in% midwest_states) %>%
-  mutate(date=as.Date(date)) %>%
+read_csv("Data/us_covid.csv") %>% 
+  as_tbl_time(date) %>%
+  filter(date>=as.Date("2020-04-01") & 
+           date<=as.Date("2020-04-30") & 
+           state %in% midwest_states) %>%
   select(-fips) %>%
   pivot_longer(cols = c("cases", "deaths"), names_to= c("type")) %>%
-  dplyr::group_by(state, type) %>%
-  ggplot(aes(date, value,  group=1))+
+  dplyr::group_by(state) %>%
+  ggplot(aes(date, value, group=state))+
     geom_line(aes(color=state))+
-    geom_point(aes(shape=type)) +
+    geom_point(aes(shape=type), size=0.5, alpha=0.7) +
     facet_wrap(~type, scales="free_y")+
     theme_classic() +
-    theme(axis.text.x = element_text(angle = 90, hjust=1, vjust=0.5))
+    theme(axis.text.x = element_text(angle = 90, hjust=1, vjust=0.5))+
+    xlab("Date")+
+    ylab("Count")
 
